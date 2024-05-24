4a302eaef7cfe326722222b9af371c009ae92f82
sgrunber <160881864+sgrunber@users.noreply.github.com>
2024-05-24 10:24:06 +0200

    final test and docstrings on the notebook

--- src/project_ppchem_tools_kit/update_clock.py
+++ src/project_ppchem_tools_kit/update_clock.py
@@ -4,13 +4,14 @@ from datetime import datetime
 
 
 
-def update_clock():
+def update_clock(window, clock_label, date_label):
     """Updates the clock label with the current time and date every second."""
     current_time = datetime.now().strftime('%H:%M:%S')
     current_date = datetime.now().strftime('%Y-%m-%d')
     clock_label.config(text=current_time)
     date_label.config(text=current_date)
-    window.after(1000, update_clock)
+    window.after(1000, update_clock, window, clock_label, date_label)
+
 
 date_label = tk.Label(window, text="", font=("Times New Roman", 20), bg="#1B262C", fg="#FFFFFF")
 date_label.place(x=890, y=10)
